cb1afbcc31b398637f751f409203dbb9ad2498df
Gregorio Santos <gregoriohd@gmail.com>
2023-01-07 19:37:12 -0300

    configurando as rotas do endpoint currency

---
+++ backend/src/rotas.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {
+    obterCotacoes,
+    obterCotacaoPorCodigo} = require('./controllers/currencys');
+
+const rotas = express();
+
+//currency
+rotas.get('/currency', obterCotacoes);
+rotas.get('/currency/:codigo', obterCotacaoPorCodigo);
+
+module.exports = rotas;
