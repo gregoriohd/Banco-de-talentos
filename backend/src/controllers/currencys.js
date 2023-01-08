b46a95f1a0fe60182bdcfaa2cefc39e96b021713
Gregorio Santos <gregoriohd@gmail.com>
2023-01-08 18:45:38 -0300

    atualizando endpoint currency

--- backend/src/controllers/currencys.js
+++ backend/src/controllers/currencys.js
@@ -4,25 +4,27 @@ const baseUrl = 'https://economia.awesomeapi.com.br/all/'
 
 
 const obterCotacoes = (req, res)=>{
-   axios.get(baseUrl)
-   .then(resp => {return res.status(200).json(resp.data)})
-   .catch((err)=>{
-        return res.status(500).json({menssage: "Ops! deu errado"});
-   })
+
+      const { codigo } = req.params;
+      if(codigo){
+          axios.get(`${baseUrl}${codigo}`)
+          .then( resp => {
+               return res.status(200).json(resp.data)})
+          .catch((err)=>{
+               return res.status(404).json({menssage: "Cotação não encontrada. Por favor verifique o codigo informado para continuar"});
+          })
+     }else{
+          axios.get(baseUrl)
+          .then(resp => {return res.status(200).json(resp.data)})
+          .catch((err)=>{
+               return res.status(500).json({menssage: "Ops! deu errado"});
+          })
+     }
+
+  
 };
 
-const obterCotacaoPorCodigo = (req, res)=>{
-    const { codigo } = req.params;
-    axios.get(`${baseUrl}${codigo}`)
-   .then( resp => {
-     return res.status(200).json(resp.data)})
-   .catch((err)=>{
-        return res.status(404).json({menssage: "Cotação não encontrada. Por favor verifique o codigo informado para continuar"});
-   })
-   
-};
 
 module.exports ={
-    obterCotacoes,
-    obterCotacaoPorCodigo
+    obterCotacoes
 }
