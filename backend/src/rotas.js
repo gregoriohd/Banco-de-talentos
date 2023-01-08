555b8b540ed16468e6c33421c56021d540e7aabf
Gregorio Santos <gregoriohd@gmail.com>
2023-01-08 18:48:47 -0300

    corrigindo rotas currency e adicionando rotas de products

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const {
-    obterCotacoes,
-    obterCotacaoPorCodigo} = require('./controllers/currencys');
-const { obterProdutos } = require('./controllers/products');
+    obterCotacoes} = require('./controllers/currencys');
+const { obterProdutos, cadastrarProduto } = require('./controllers/products');
 
 const rotas = express();
 
 //currency
 rotas.get('/currency', obterCotacoes);
-rotas.get('/currency/:codigo', obterCotacaoPorCodigo);
+rotas.get('/currency/:codigo', obterCotacoes);
 
 //products
 rotas.get('/products', obterProdutos);
+rotas.get('/products/:id', obterProdutos);
+rotas.post('/products', cadastrarProduto);
 
 module.exports = rotas;
